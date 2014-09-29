e18f1e37d1465965f584d41736e1e5153991dc05
sarah1985 <sarahmleavy@gmail.com>
2014-09-28 20:26:42 -0400

    lesson 4
    task 4

--- task_04.py
+++ task_04.py
@@ -3,5 +3,28 @@
 """Task 04: Looping Mathematical Calculations"""
 
 import data
+
 print data.TRANSACTIONS
+TRANSACTIONS = data.TRANSACTIONS
+
+NEW_LIST = [sum(amt) for amt in TRANSACTIONS]
+print NEW_LIST
+
+TOTAL = 0
+MINIMUM = 1000000000
+MAXIMUM = 0
+for num in NEW_LIST:
+
+    if num < MINIMUM:
+        MINIMUM = num
+
+    if num > MAXIMUM:
+        MAXIMUM = num
+
+    TOTAL += num
+print '''
+Minimum Daily Transaction Total: {}
+Maximum Daily Transaction Total: {}
+Total Transaction Total: {}'''.format(MINIMUM, MAXIMUM, TOTAL)
+
 
